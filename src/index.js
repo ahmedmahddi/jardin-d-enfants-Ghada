862ad45c39f8bd86ee1b8b935a1198faebb53f2d
ahmedmahddi <ahmed.mahdi@enis.tn>
2024-05-16 11:36:36 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import App from "./App";
 import { AuthContextProvider } from "./contexts/AuthContext";
 import "./styles/tailwind.css";
 import ErrorBoundary from "./components/common/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </AuthContextProvider>
+    <ThemeProvider>
+      <AuthContextProvider>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </AuthContextProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
