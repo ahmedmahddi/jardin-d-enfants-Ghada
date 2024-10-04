f10cc60e603a97c12384556659d18aa655e34de3
ahmedmahddi <155826291+ahmedmahddi@users.noreply.github.com>
2024-10-04 13:52:47 +0100

    Remove server folder from repository and ignore it

--- server/services/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./auth.service.js";
-export * from "./enrollment.service.js";
-export * from "./staff.service.js";
-export * from "./user.service.js";
-export * from "./invoice.service.js";
